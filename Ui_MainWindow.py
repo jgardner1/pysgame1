437f4b2e414dd2f72f96dcbc80e4a0c12ac13cc3
Jonathan Gardner <jgardner@jonathangardner.net>
2014-03-03 22:20:59 -0800

    A good checkpoint
    I have some fairly basic stuff built in. Citizens grow 20% each turn.

---
+++ Ui_MainWindow.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'MainWindow.ui'
+#
+# Created: Mon Mar 03 22:18:34 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.output = QtGui.QTextBrowser(self.centralwidget)
+        self.output.setObjectName("output")
+        self.verticalLayout.addWidget(self.output)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.input = QtGui.QLineEdit(self.centralwidget)
+        self.input.setObjectName("input")
+        self.horizontalLayout.addWidget(self.input)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtGui.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuGame = QtGui.QMenu(self.menubar)
+        self.menuGame.setObjectName("menuGame")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionQuit = QtGui.QAction(MainWindow)
+        self.actionQuit.setObjectName("actionQuit")
+        self.actionOpen = QtGui.QAction(MainWindow)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QtGui.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionSaveAs = QtGui.QAction(MainWindow)
+        self.actionSaveAs.setObjectName("actionSaveAs")
+        self.actionNew = QtGui.QAction(MainWindow)
+        self.actionNew.setObjectName("actionNew")
+        self.actionEndTurn = QtGui.QAction(MainWindow)
+        self.actionEndTurn.setObjectName("actionEndTurn")
+        self.actionShowStatus = QtGui.QAction(MainWindow)
+        self.actionShowStatus.setObjectName("actionShowStatus")
+        self.menuFile.addAction(self.actionNew)
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionSaveAs)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionQuit)
+        self.menuGame.addAction(self.actionEndTurn)
+        self.menuGame.addSeparator()
+        self.menuGame.addAction(self.actionShowStatus)
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuGame.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QObject.connect(self.actionQuit, QtCore.SIGNAL("activated()"), MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("MainWindow", "Input", None, QtGui.QApplication.UnicodeUTF8))
+        self.input.setToolTip(QtGui.QApplication.translate("MainWindow", "<html><head/><body><p>Enter some commands here</p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuFile.setTitle(QtGui.QApplication.translate("MainWindow", "&File", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuGame.setTitle(QtGui.QApplication.translate("MainWindow", "&Game", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionQuit.setText(QtGui.QApplication.translate("MainWindow", "&Quit", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionQuit.setShortcut(QtGui.QApplication.translate("MainWindow", "Ctrl+Q", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionOpen.setText(QtGui.QApplication.translate("MainWindow", "&Open", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionOpen.setShortcut(QtGui.QApplication.translate("MainWindow", "Ctrl+O", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionSave.setText(QtGui.QApplication.translate("MainWindow", "&Save", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionSave.setShortcut(QtGui.QApplication.translate("MainWindow", "Ctrl+S", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionSaveAs.setText(QtGui.QApplication.translate("MainWindow", "Save &As", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionSaveAs.setShortcut(QtGui.QApplication.translate("MainWindow", "Backspace", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionNew.setText(QtGui.QApplication.translate("MainWindow", "&New", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionNew.setShortcut(QtGui.QApplication.translate("MainWindow", "Ctrl+N", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionEndTurn.setText(QtGui.QApplication.translate("MainWindow", "&End Turn", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionShowStatus.setText(QtGui.QApplication.translate("MainWindow", "&Show Status", None, QtGui.QApplication.UnicodeUTF8))
+
--- pysgame1.py
+++ pysgame1.py
@@ -1,15 +1,60 @@
 #!/usr/bin/env python
 
 from PySide import QtCore, QtGui, QtUiTools
-from main_ui import Ui_MainWindow
+from Ui_MainWindow import Ui_MainWindow
+
+class Game(object):
+    def __init__(self):
+        self.citizens = 20
+        self.gold = 0
+        self.buildings = {}
+
+
+    def end_turn(self):
+        self.citizens += int(self.citizens*0.2+0.5)
 
 class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
+        self.game = Game()
+
         # Load main.ui
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.ui.input.returnPressed.connect(self.new_input)
+
+        self.show_status()
+
+        self.ui.actionEndTurn.activated.connect(self.end_turn)
+        self.ui.actionShowStatus.activated.connect(self.show_status)
+
+    def end_turn(self):
+        self.game.end_turn()
+        self.show_status()
+
+    def new_input(self):
+        text = self.ui.input.text()
+        self.ui.input.setText("")
+
+        self.ui.output.append("You entered: "+text)
+        self.game.end_turn()
+        self.show_status()
+
+    def show_status(self):
+        self.ui.output.append("You have {} citizens, {} gold and {} buildings.".format(
+                self.game.citizens or 'no',
+                self.game.gold or 'no',
+                self.game.buildings,
+            ))
+
+        
+
+
+
+    
+    
 
 
 
