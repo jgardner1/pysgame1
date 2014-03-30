179506d548b543237c13e0157e167e30905571d3
Jonathan Gardner <jgardner@jonathangardner.net>
2014-03-29 23:45:47 -0700

    Comments on MainWindow code.

--- pysgame1/ui/MainWindow.py
+++ pysgame1/ui/MainWindow.py
@@ -14,24 +14,42 @@ class MainWindow(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # When they press enter/return on the input textbox, we will process
+        # that input.
         self.ui.input.returnPressed.connect(self.new_input)
 
+        # Whenever they select 'end turn' from the menu, we will end the turn.
         self.ui.actionEndTurn.activated.connect(self.game.end_turn)
+
+        # Whenever they select 'show status' from the menu, we will show the
+        # status.
         self.ui.actionShowStatus.activated.connect(self.game.show_status)
 
+        # Set the people tree up with the data.
         self.ui.peopleTreeView.setModel(PeopleTreeModel(self.game))
+
+        # This will make displaying lots of people much faster.
         self.ui.peopleTreeView.setUniformRowHeights(True)
         #self.ui.peopleTreeView.setSortingEnabled(True)
 
+        # Whenever the end turn button is clicked, end the turn.
         self.ui.endTurnButton.clicked.connect(self.game.end_turn)
 
+        # When the game has output, have it display in the output window.
         self.game.output.connect(self.ui.output.append)
 
     def new_input(self):
+        # Gather the text.
         text = self.ui.input.text()
+
+        # Set the text input to blank for the next command.
         self.ui.input.setText("")
 
         self.ui.output.append("You entered: "+text)
+
+        # TODO: Connect the input to the game object.
+
+        # End the turn. TODO: Not every input will end the turn!
         self.game.end_turn()
 
 
